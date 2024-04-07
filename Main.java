f57d869a90c6529ad51a7e841f855d44fe613a0d
The_Pi$ith <pisith@Pisiths-MBP.bbrouter>
2024-04-07 23:06:51 +0700

    implemented comparable objects

---
+++ Main.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+// Mocking
+class MockSong {
+
+    public static List<String> getSongList() {
+        List<String> songs = new ArrayList<>();
+        songs.add("somersault");
+        songs.add("cassidy");
+        songs.add("10$");
+        songs.add("havana");
+        songs.add("cassidy");
+        songs.add("50 Ways");
+        return songs;
+    }
+}
+
+class Song implements Comparable <Song>{
+    private String title;
+    private String artist;
+    private int bpm;
+
+    public Song(String title, String artist, int bpm) {
+        this.title = title;
+        this.artist = artist;
+        this.bpm = bpm;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getBpm() {
+        return bpm;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", bpm=" + bpm +
+                '}';
+    }
+
+    // Comparable method for using in sort()
+    public int compareTo(Song s) {
+        return this.title.compareTo(s.getTitle());
+    }
+}
+
+class MockSongV2 {
+
+    public static List<Song> getSongList() {
+        List<Song> songs = new ArrayList<Song>();
+        songs.add(new Song("somersault", "zero 7", 147));
+        songs.add(new Song("cassidy", "grateful dead", 158));
+        songs.add(new Song("$10", "hitchhiker", 140));
+        songs.add(new Song("havana", "cabello", 105));
+        songs.add(new Song("Cassidy", "grateful dead", 158));
+        songs.add(new Song("50 ways", "simon", 102));
+        return songs;
+    }
+}
+
+
+
+class Main {
+    public static void main(String[] args) {
+//        List<String> songList = MockSong.getSongList();
+//        System.out.println("Orginal List ====>");
+//        System.out.println(songList);
+//
+//        Collections.sort(songList);
+//        System.out.println("Collection Sorted List ====>");
+//        System.out.println(songList);
+
+        // =========
+
+        List<Song> songListV2 = MockSongV2.getSongList();
+
+        System.out.println("Orginal List ====>");
+        System.out.println(songListV2);
+
+        Collections.sort(songListV2);
+        System.out.println(songListV2);
+
+    }
+}
